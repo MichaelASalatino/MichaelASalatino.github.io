5b49c92d0cc8294b57f4b662f065a10c7c20277e
Michael Salatino <msalatino@student.otis.edu>
2018-12-03 17:21:31 -0800

    p7 done

--- p7/lightbox.js
+++ p7/lightbox.js
@@ -12,6 +12,15 @@ $('a').click(function(){
     //show modal with a fade of .05s
     $('div.modal').fadeIn(500)
     
+    // fill content with a image
+    $('div.modal-content').html(
+        $(this).html()
+    )
     //stop link from going to the next page
     event.preventDefault()
+})
+
+// close light box 
+$('div.modal-close').click(function() {
+    $('div.modal').fadeOut(500)
 })
